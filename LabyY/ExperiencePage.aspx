96853547e9ca35b484199756d0b0fadd5c7c2982
user <user@T60KB01IT008054>
2020-10-04 19:52:01 +0300

    Added Experience page

---
+++ LabyY/ExperiencePage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExperiencePage.aspx.cs" Inherits="LabyY.ExperiencePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Untitiled Page</title>
+</head>
+<body>
+    <form id="ExperienceForm" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Введите Ваше последнее место работы"></asp:Label>
+        <asp:Panel ID="Places" runat="server">
+            <asp:TextBox ID="first" runat="server"></asp:TextBox>
+            <br />
+            <br />
+        </asp:Panel>
+        <asp:Button ID="AddButton" runat="server" Text="Еще" OnCommand="AddExperience" CommandName="Add" />
+    </form>
+</body>
+</html>
---
+++ LabyY/ExperiencePage.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LabyY
+{
+    public partial class ExperiencePage : System.Web.UI.Page
+    {
+        int i = 0;
+        int max = 10;
+        TextBox[] tb = new TextBox[10];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AddExperience(object sender, EventArgs e)
+        {
+            if (i < max)
+            {
+                TextBox textBox = new TextBox()
+                {
+                    ID = "textBox" + i
+                };
+                tb[i] = textBox;
+                i++;                                                   
+            }
+            for(int a =0; a < max; a++)
+            {
+                if (tb[a] != null)
+                {
+                    Places.Controls.Add(tb[a]);
+                    Label label = new Label();
+                    Places.Controls.Add(label);
+                }
+                else break;
+            }
+        }
+    }
+}
