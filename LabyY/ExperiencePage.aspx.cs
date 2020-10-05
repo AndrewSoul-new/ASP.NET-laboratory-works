8c6f6d080f646b843f0aae849ffb0fb7026e3ed5
AndrewSoul-new <52050091+AndrewSoul-new@users.noreply.github.com>
2020-10-05 15:54:39 +0300

    added array

--- LabyY/ExperiencePage.aspx.cs
+++ LabyY/ExperiencePage.aspx.cs
@@ -11,7 +11,7 @@ namespace LabyY
     {
         static int i = 0;
         static int max = 10;
-        TextBox tb;
+        TextBox[] tb = new TextBox[10];
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -21,12 +21,13 @@ namespace LabyY
         {
             if(i < max)
             {              
-                for(int j = 0;j<=i;j++)
-                {
-                    tb = new TextBox() { ID = "textbox" + i };
-                    Places.Controls.Add(tb);
+                for(int j = i;j<=i;j++)
+                { 
+                    TextBox textBox = new TextBox() { ID = "textbox" + i, Text = i.ToString() };                    
+                    tb[i] = textBox;
+                    Places.Controls.Add(tb[i]);
                     Label label = new Label() { Text = "<br/> <br/>" };
-                    Places.Controls.Add(label);                    
+                    Places.Controls.Add(label);                  
                 }
                 i++;
             }
